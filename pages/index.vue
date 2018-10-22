5e855124d09828973b539f6ddf95de62af62188d
ANTONIO CORDEIRO <hbc.antonio@gmail.com>
2018-10-22 17:02:48 -0400

    [master] Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        high-touch
+      </h1>
+      <h2 class="subtitle">
+        DC&amp;ES high touch forms
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+const foo = async (value, timer) => {
+  return new Promise((resolve, reject) => {
+    try {
+      setTimeout(() => {
+        resolve(value * 2)
+      }, timer)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+const bar = async () => {
+  console.log(await foo(5, 5000))
+}
+
+bar()
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
