3d3136f434b7030b7f99e94e24819605132f04fc
Cordeiro, Antonio <antonio.cordeiro@investorsgroup.com>
2018-12-05 10:50:15 -0500

    wip

--- services/api.js
+++ services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 export default () => {
+	console.log('baseURL', process.env.BASE_URL)
+
 	return axios.create({
 		baseURL: !process.env.BASE_URL
 			? 'http://localhost:8080'
